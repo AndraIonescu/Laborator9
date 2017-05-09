56ba9380b43d353dc580ddd4dbff9c12c1373d3a
Student User <student@cti.pub.ro>
2017-05-09 11:14:43 +0300

    Implementare

--- labtasks/NgnSip/app/src/main/java/ro/pub/cs/systems/eim/lab09/ngnsip/general/Constants.java
+++ labtasks/NgnSip/app/src/main/java/ro/pub/cs/systems/eim/lab09/ngnsip/general/Constants.java
@@ -8,13 +8,13 @@ public interface Constants {
 
     // TODO exercise 4
     // fill in the USERNAME, IDENTITY_IMPI, IDENTITY_PASSWORD, DOMAIN, NETWORK_REALM
-    final public static String USERNAME = "...";
-    final public static String IDENTITY_IMPI = "...";
-    final public static String IDENTITY_PASSWORD = "...";
-    final public static String DOMAIN = "...";
+    final public static String USERNAME = "andradenis.ionescu";
+    final public static String IDENTITY_IMPI = "smityity";
+    final public static String IDENTITY_PASSWORD = "nFtZB9V5WHH6Tvm5";
+    final public static String DOMAIN = "smityity.onsip.com";
     final public static String NETWORK_PCSCF_HOST = "sip.onsip.com";
     final public static int NETWORK_PCSCF_PORT = 5060;
-    final public static String NETWORK_REALM = "...";
+    final public static String NETWORK_REALM = "smityity.onsip.com";
 
     final public static boolean NETWORK_USE_3G = true;
     final public static int NETWORK_REGISTRATION_TIMEOUT = 3600;
